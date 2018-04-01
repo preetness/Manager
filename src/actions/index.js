5d517df69b3f0c37c02716b1a9d1145091791f89
Preet Dha <manpreetdha+github@gmail.com>
2018-03-31 18:37:42 -0700

    Refactor code for successful login by creating a helper function; add error handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+} from './types';
 import firebase from 'firebase';
 
 export const emailChanged = text => {
@@ -20,8 +25,26 @@ export const loginUser = ({ email, password }) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      .then(user => successfulLogin(dispatch, user))
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(user => successfulLogin(dispatch, user))
+          .catch(() => failedLogin(dispatch));
       });
   };
+};
+
+const successfulLogin = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const failedLogin = dispatch => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
 };
