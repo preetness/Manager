02c3c0edb5d9a52eec3f3c7214ec2f7153d37809
Preet Dha <manpreetdha+github@gmail.com>
2018-04-01 22:37:05 -0700

    Use a Picker component to allow user to pick a day of the week to add to employee's shift

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react';
+import { View } from 'react-native';
 
 const CardSection = props => (
-  <View style={styles.containerStyle}>{props.children}</View>
+  <View style={[styles.containerStyle, props.style]}>{props.children}</View>
 );
 
 const styles = {
   containerStyle: {
     borderBottomWidth: 1,
     padding: 5,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    flexDirection: "row",
-    borderColor: "#ddd",
-    position: "relative"
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
   }
 };
 
